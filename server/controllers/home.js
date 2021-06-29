6719266741c7d3f3d8faf9be2f3ed0c013113fb5
Geraldine <hello@geraldiner.com>
2021-06-28 21:47:49 -1000

    add cleaner routes

---
+++ server/controllers/home.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const twitch = require("../util/twitch_api")();
+const twitchInfo = require("../util/twitch");
+
+module.exports = {
+  getIndex: (req, res) => {
+    res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+  },
+  getStreams: async (req, res) => {
+    const resp = await twitch.helix.streams.getStreams({
+      game: twitchInfo.gameIds["Science & Technology"],
+      limit: "100",
+    });
+
+    const data = await resp.data;
+    const streams = [];
+    data.map((d) => {
+      let obj = {
+        userId: d.userId,
+        userDisplayName: d.userDisplayName,
+        language: d.language,
+        tagIds: d.tagIds,
+        thumbnailUrl: d.thumbnailUrl,
+        streamTitle: d.title,
+        viewers: d.viewers,
+      };
+      streams.push(obj);
+    });
+    return res.status(200).send(JSON.stringify(streams));
+  },
+};
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,37 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path");
-const twitch = require("./util/twitch_api")();
-const twitchInfo = require("./util/twitch");
-const { tagIds } = require("./util/twitch");
+const homeController = require("../controllers/home");
 
 // @desc    Get the home page
 // @route   GET /
-router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
-});
+router.get("/", homeController.getIndex);
 
 // @desc    Get list of streams
 // @route   GET /streams
-router.get("/streams", async (req, res) => {
-  const resp = await twitch.helix.streams.getStreams({
-    game: twitchInfo.gameIds["Science & Technology"],
-    limit: "100",
-  });
+router.get("/streams", homeController.getStreams);
 
-  const data = await resp.data;
-  const streams = [];
-  data.map((d) => {
-    let obj = {
-      userId: d.userId,
-      userDisplayName: d.userDisplayName,
-      language: d.language,
-      tagIds: d.tagIds,
-      thumbnailUrl: d.thumbnailUrl,
-      streamTitle: d.title,
-      viewers: d.viewers,
-    };
-    streams.push(obj);
-  });
-  return res.status(200).send(JSON.stringify(streams));
-});
+module.exports = router;
