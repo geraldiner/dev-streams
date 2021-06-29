deffd5baa848cc533cdd4a625cab1e249c2a6777
Geraldine <hello@geraldiner.com>
2021-06-28 21:08:59 -1000

    fix endpoint

--- client/src/components/Streams.js
+++ client/src/components/Streams.js
@@ -3,11 +3,15 @@ import axios from "axios";
 import Stream from "./Stream";
 
 const Streams = () => {
+  const STREAMS_URI =
+    process.env.NODE_ENV !== "production"
+      ? "http://localhost:8888/streams"
+      : "https://dev-streams.herokuapp.com/movies";
   const [streams, setStreams] = useState([]);
 
   useEffect(() => {
     async function fetchStreams() {
-      const res = await axios.get("http://localhost:8888/streams");
+      const res = await axios.get(STREAMS_URI);
       const data = await res.data;
       setStreams(data);
     }
